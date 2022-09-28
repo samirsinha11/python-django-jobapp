38ff2356a94e50a2606cdd84d12e9a81c58980c0
Samir <samirsinha11@gmail.com>
2022-09-28 17:04:09 +0530

    Static file  commit

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include('app.urls')),
     path('', include('subscribe.urls')),
     path('uploads/', include('uploadapp.urls')),
-    path('job/<job_id>', include('app.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

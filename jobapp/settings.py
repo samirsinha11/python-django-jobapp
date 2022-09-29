7828540a52b2fcf0a7edd5319d3b9ca85f63f35f
Samir <samirsinha11@gmail.com>
2022-09-29 15:02:41 +0530

    RDBMS

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = "jobapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.env("DB_NAME"),
+        'USER': os.env("DB_USER"),
+        'PASSWORD': os.env("DB_PASSWORD"),
+        'HOST': os.env("DB_HOST"),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
